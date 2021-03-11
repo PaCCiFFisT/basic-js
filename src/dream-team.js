146a73bfaa230ed2f182c95e5ed05cd3f6c9682d
PaCCiFFisT <pacciffist@gmail.com>
2021-03-11 22:02:20 +0200

    feat: dream-team complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let sortMem = [],
+    teamName = '';
+  console.log(typeof members)
+  if (typeof members != 'object' || members === null || Array.isArray(members) === false) { return false };
+  sortMem = members.filter(element => typeof element === 'string').map(element => element.trim());
+  console.log(sortMem)
+  sortMem.forEach((element, i) => {
+    if (typeof element != 'string') { return false };
+    teamName = teamName + element.charAt(0);
+    console.log(typeof teamName)
+  })
+  let teamNameArr = teamName.toUpperCase().split('');
+  teamNameArr.sort().join('');
+  if (teamName.length >= 1) {
+    return teamNameArr.sort().join('');
+  } else {
+    return false;
+  }
 };

3c5b1920027b2e7c603c121f1ce170f5e47d5ca3
PaCCiFFisT <pacciffist@gmail.com>
2021-03-28 14:18:58 +0300

    refactor: delete console log str

--- src/what-season.js
+++ src/what-season.js
@@ -1,8 +1,5 @@
-
-
 module.exports =
   function getSeason(date) {
-    //console.log(date.getMonth())
     if (date === undefined) {
       return 'Unable to determine the time of year!';
     }
@@ -10,16 +7,12 @@ module.exports =
       throw new Error
     }
     if (date.getMonth() >= 2 && date.getMonth() <= 4) {
-      console.log('spring')
       return 'spring';
     } else if (date.getMonth() >= 5 && date.getMonth() <= 7) {
-      console.log('summer')
       return 'summer';
     } else if (date.getMonth() >= 8 && date.getMonth() <= 10) {
-      console.log('authumn')
       return 'fall'
     } else {
-      console.log('winter')
       return 'winter'
     }
   }

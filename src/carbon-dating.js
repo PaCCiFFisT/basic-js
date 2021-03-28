5411aaa26e837117b84af3adf62ae5a53cbf9ce8
PaCCiFFisT <pacciffist@gmail.com>
2021-03-28 14:15:56 +0300

    fix: fix returning value

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,13 +5,10 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
   var sampleAct = parseFloat(sampleActivity);
-  if (typeof sampleActivity != 'string' || isNaN(sampleAct) || sampleAct <= 0 || sampleAct > MODERN_ACTIVITY) {
-    console.log(sampleAct)
-    return false;
-  } else if (typeof parseFloat(sampleActivity) != 'number' || sampleAct <= 0 || sampleAct > MODERN_ACTIVITY) {
+  if (typeof sampleActivity != 'string' || typeof parseFloat(sampleActivity) != 'number' || isNaN(sampleAct) || sampleAct <= 0 || sampleAct > MODERN_ACTIVITY) {
     return false;
   } else {
     var age = (Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD)
-    console.log(Math.ceil(age))
+    return Math.ceil(age)
   }
 };

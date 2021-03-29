e4992e2ff2a79accd8977c39a83d0212e378d624
PaCCiFFisT <pacciffist@gmail.com>
2021-03-30 00:11:29 +0300

    feat: complete transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
-const CustomError = require("../extensions/custom-error");
+module.exports =
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  function transform(arr) {
+
+    if (arr.length === 0) {
+      return [];
+    }
+    const doubleN = '--double-next',
+      doubleP = '--double-prev',
+      discardN = '--discard-next',
+      discardP = '--discard-prev';
+    let solution = [],
+      flagAdd = false,
+      flagRemove = false;
+    for (var i = 0; i < arr.length; i++) {
+      if (flagRemove === true) {
+        flagRemove = false;
+        continue;
+      } else if (flagAdd === true) {
+        flagAdd = false;
+        solution.push(arr[i]);
+        solution.push(arr[i]);
+        continue;
+      }
+      if (arr[i] != doubleN && arr[i] != doubleP && arr[i] != discardN && arr[i] != discardP) {
+        solution.push(arr[i])
+      } else if (arr[i] === discardP) {
+        if (i >= 1 && arr[i - 2] != discardN) {
+          solution.pop()
+        }
+      } else if (arr[i] === doubleP) {
+        if (i >= 1 && arr[i - 2] != discardN) {
+          solution.push(arr[i - 1])
+        }
+      } else if (arr[i] === doubleN) {
+        flagAdd = true
+      } else if (arr[i] === discardN) {
+        flagRemove = true
+      }
+
+
+    };
+    return solution;
+  }
